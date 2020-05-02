ecee1219b484c4083005534a29b408b8db98dd6e
建中貓貓男 <54308739+MaowMan@users.noreply.github.com>
2020-05-02 14:38:42 +0800

    2020/5/2

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", event => {
+    setInterval(function() {
+        now_time = new Date()
+    }, 200)
     const toXiaoUrl = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/StationTimeTable/TRTC?$filter=StationID%20%20eq%20'G03A'%20and%20DestinationStaionID%20eq%20'G03'&$format=JSON";
     const fromXiaoUrl = "https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/StationTimeTable/TRTC?$filter=StationID%20%20eq%20'G03'%20and%20DestinationStaionID%20eq%20'G03A'&$format=JSON";
     [toXiaoUrl, fromXiaoUrl].forEach(url => {
@@ -6,24 +9,48 @@ document.addEventListener("DOMContentLoaded", event => {
             .then(response => {
                 return response.json()
             })
-            .then(res => {
-                if (res[0].StationID == "G03A") {
+            .then(data => {
+                let res, index
+                if (now_time.getDay() === 0 || now_time.getDay() === 6) {
+                    index = 1
+                } else {
+                    index = 0
+                }
+                res = data[index].Timetables.map(string => {
+                    let iso_string_now = now_time.toISOString()
+                    let final_string = iso_string_now.split("T")[0] + "T" + string.DepartureTime + ":00.000Z"
+                    let DateObj = new Date(final_string)
+                    return DateObj
+                })
+                if (data[0].StationID == "G03A") {
                     fromXiaoData = res
-                } else if (res[0].StationID == "G03") {
+                } else if (data[0].StationID == "G03") {
                     toXiaoData = res
 
                 } else {
                     console.log("err")
-                    console.log(res)
+                    console.log(data)
                 }
             })
             .catch(err => {
                 console.log(err)
             })
-    })
+            .finally(event => {
+                setInterval(function() {
 
+                }, 200)
+            })
+    })
 })
 
 let toXiaoData
 
 let fromXiaoData
+
+let now_time = new Date()
+
+let nxt_train = null
+
+let mainObj = new Vue({
+
+})
